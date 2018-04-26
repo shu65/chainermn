5980aa62a86c4f0d35d3d2d327b8f9a3272e4126
Shuji Suzuki <ssuzuki@preferred.jp>
2018-04-26 09:59:43 +0900

    fix bug which occurs when using python 2.7

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import multiprocessing
+import platform
 import pytest
 
 
@@ -8,8 +9,9 @@ def dummy_func():
 
 @pytest.fixture(scope='session', autouse=True)
 def scope_session():
-    multiprocessing.set_start_method('forkserver')
-    p = multiprocessing.Process(target=dummy_func)
-    p.start()
-    p.join()
+    if platform.python_version_tuple()[0] >= 3:
+        multiprocessing.set_start_method('forkserver')
+        p = multiprocessing.Process(target=dummy_func)
+        p.start()
+        p.join()
     yield
