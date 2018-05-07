7fcd98b7ee822ddd739e7b4cf4dab9ef60eff36e
Shuji Suzuki <ssuzuki@preferred.jp>
2018-05-07 19:12:48 +0900

    refacotring

--- chainermn/communicators/_communication_utility.py
+++ chainermn/communicators/_communication_utility.py
@@ -74,6 +74,23 @@ def init_comms(mpi_comm, intra_rank, intra_size, inter_rank, use_nccl=True):
     else:
         return intra_mpi_comm, inter_mpi_comm
 
+def init_intra_mpi_comm(mpi_comm, intra_rank, inter_rank):
+    return mpi_comm.Split(inter_rank, intra_rank)
+
+
+def init_inter_mpi_comm(mpi_comm, intra_rank, inter_rank):
+    return mpi_comm.Split(intra_rank, inter_rank)
+
+
+def init_nccl_comm(mpi_comm):
+    from chainermn import nccl
+    if mpi_comm.rank == 0:
+        nccl_comm_id = nccl.get_unique_id()
+    else:
+        nccl_comm_id = None
+    nccl_comm_id = mpi_comm.bcast(nccl_comm_id)
+    return nccl.NcclCommunicator(mpi_comm.size, nccl_comm_id, mpi_comm.rank)
+
 
 def inter_allreduce_gpu(
         inter_mpi_comm, size, gpu_buffer_a, gpu_buffer_b,
